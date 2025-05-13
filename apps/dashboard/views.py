423d6f8abcc11271848467df87b29ddd0584b74d
dineshpaliwal-coder <68260470+dineshpaliwal-coder@users.noreply.github.com>
2025-05-13 21:18:38 +0530

    Add apps/dashboard/views.py 1747151305111

---
+++ apps/dashboard/views.py
@@ -0,0 +1,11 @@
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Add dashboard data to context
+        context['page_title'] = 'Dashboard'
+        return context
